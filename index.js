448e9c951c77cd3cfa7951d0e44b0442eadc77b3
GeoffreyMokua <geoffrey.nyakundi@student.moringaschool.com>
2022-08-31 07:59:07 +0300

    POST request not working

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 // Add your code here
-function submitData(name, email){
-    console.log("here")
-    const config = {
-        method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-            'Accept': 'application/json',
+function submitData(name, email) {
+	return fetch("http://localhost:3000/users", {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			Accept: "application/json",
+		},
+		body: JSON.stringify({
+			name,
+			email,
+		}),
+	})
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (object) {
+			document.body.innerHTML = object["id"];
+		})
+		.catch(function (error) {
+			document.body.innerHTML = error.message;
+		});
 
-        },
-        body: JSON.stringify({
-            name: "Steve",
-            email: "steve@steve.com",
-        }),
-    };
 
 
     fetch("http://localhost:3000/users", config)
